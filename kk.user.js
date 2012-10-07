ba1737b24ce9a9219d290e2c9be3f08cf0f45361
rvclayton <rclayton@monmouth>
2012-10-07 14:15:05 -0400

    drop more stuff along the top and left margin.

--- kk.user.js
+++ kk.user.js
@@ -2,7 +2,13 @@
 // @name	cooltools margin reducer
 // @namespace	file:///home/rclayton/projects/greasemonkey/scripts
 // @description Get rid of the left margin.
-// @include	HTTP://www.kk.org/cooltools/*
+// @include	HTTP://kk.org/cooltools/*
+// @require     HTTP://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
 document.getElementById("CTcontentcolumns").style.marginLeft = 0
+
+$.each(
+  [ 'logo-login', 'nav', 'categoriescol', 'yellowbox' ],
+  function(_, v) { $('#' + v).remove(); }
+  )
