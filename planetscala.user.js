4884e3cde4e3ed1d8ea9d2d9df017ab1368a7258
rvclayton <rclayton@monmouth>
2013-05-19 17:57:52 -0400

    created

---
+++ planetscala.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name	planet scala right-margin eliminator
+// @namespace	file:///home/rclayton/projects/greasemonkey/scripts
+// @description Set the right margin to zero width.
+// @include	http://www.planetscala.com/*
+// @require     HTTP://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
+// ==/UserScript==
+
+document.body.style.marginRight = 0
