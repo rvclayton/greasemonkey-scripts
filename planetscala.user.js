15e633f71814aa8097cf43225d538d30d5e0cad0
rvclayton <rclayton@monmouth>
2013-05-19 20:53:05 -0400

    get rid of the planet haskell right margin too

--- planetscala.user.js
+++ planetscala.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
-// @name	planet scala right-margin eliminator
+// @name	planet blog right-margin eliminator
 // @namespace	file:///home/rclayton/projects/greasemonkey/scripts
 // @description Set the right margin to zero width.
 // @include	http://www.planetscala.com/*
+// @include	http://planet.haskell.org/*
 // @require     HTTP://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
 
